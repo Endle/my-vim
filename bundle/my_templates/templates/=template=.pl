d79611d879fc2b780f1caa12bdc9688768f67915
Zhenbo Li <litimetal@gmail.com>
2017-09-25 19:13:20 -0400

    vim-template no longer handled by submodule

---
+++ bundle/my_templates/templates/=template=.pl
@@ -0,0 +1,17 @@
+#! /usr/bin/env perl
+use strict;
+use warnings;
+use 5.16.3; # Always match defiance
+use feature qw(say);
+
+########################################################
+#
+# File          %FILE%.pl
+# BIOL6308      fall2017
+# Author        Zhenbo Li <li.zhenb@husky.neu.edu>
+# Created       %DATE%
+#
+########################################################
+
+
+%HERE%
